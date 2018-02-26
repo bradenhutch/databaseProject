41b23b2e96a87b9311d6fb48d761a59faf9fd3c8
Braden Hutchinson <braden.hutchinson@hotmail.com>
2018-02-26 16:20:01 -0700

    added database init file in evolutions

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,96 @@
+# --- !Ups
+
+CREATE TABLE user (
+	userId			BIGINT NOT NULL AUTO_INCREMENT,
+	username		VARCHAR(255) NOT NULL,
+	phoneNumber		VARCHAR(255) NOT NULL,
+	name			VARCHAR(255) NOT NULL,
+	email			VARCHAR(255) NOT NULL,
+	password		VARCHAR(255) NOT NULL,
+	gender			VARCHAR(255) NOT NULL,
+	admin			BOOLEAN NOT NULL,
+	PRIMARY KEY(userId)
+);
+
+CREATE TABLE payment_method (
+	paymentId		BIGINT NOT NULL AUTO_INCREMENT,
+	userId			VARCHAR(255) NOT NULL,
+	PRIMARY KEY(paymentId)
+);
+
+CREATE TABLE paypal (
+	paypalId		BIGINT NOT NULL AUTO_INCREMENT,
+	paymentId		VARCHAR(255) NOT NULL,
+	paypalEmail		VARCHAR(255) NOT NULL,
+	PRIMARY KEY(paypalId)
+);
+
+CREATE TABLE credit_card (
+	cardId			BIGINT NOT NULL AUTO_INCREMENT,
+	paymentId		VARCHAR(255) NOT NULL,
+	cardNumber		VARCHAR(255) NOT NULL,
+	expirationDate	VARCHAR(255) NOT NULL,
+	CVV				VARCHAR(255) NOT NULL,
+	PRIMARY KEY(cardId)
+);
+
+CREATE TABLE address (
+	addressId		BIGINT NOT NULL AUTO_INCREMENT,
+	userId			VARCHAR(255) NOT NULL,
+	streetNumber	VARCHAR(255) NOT NULL,
+	zipCode			VARCHAR(255) NOT NULL,
+	city			VARCHAR(255) NOT NULL,
+	PRIMARY KEY(addressId)
+);
+
+CREATE TABLE order (
+	orderId			BIGINT NOT NULL AUTO_INCREMENT,
+	userId			VARCHAR(255) NOT NULL,
+	shippingAddId	VARCHAR(255) NOT NULL,
+	billingAddId	VARCHAR(255) NOT NULL,
+	createdDate		VARCHAR(255) NOT NULL,
+	shippedDate		VARCHAR(255) NOT NULL,
+	subtotal		VARCHAR(255) NOT NULL,
+	tax				VARCHAR(255) NOT NULL,
+	total			VARCHAR(255) NOT NULL,
+	PRIMARY KEY(orderId)
+);
+
+CREATE TABLE product (
+	productId		BIGINT NOT NULL AUTO_INCREMENT,
+	price			VARCHAR(255) NOT NULL,
+	imageLocation	VARCHAR(255) NOT NULL,
+	color			VARCHAR(255) NOT NULL,
+	material		VARCHAR(255) NOT NULL,
+	dimensions		VARCHAR(255) NOT NULL,
+	weight			VARCHAR(255) NOT NULL,
+	currentStock	VARCHAR(255) NOT NULL,
+	description		TEXT NOT NULL,
+	PRIMARY KEY(productId)
+);
+
+CREATE TABLE review (
+	reviewId		BIGINT NOT NULL AUTO_INCREMENT,
+	productId		VARCHAR(255) NOT NULL,
+	reviewDate		VARCHAR(255) NOT NULL,
+	author			VARCHAR(255) NOT NULL,
+	reviewText		TEXT NOT NULL,
+);
+
+CREATE TABLE order_products (
+	orderId			VARCHAR(255) NOT NULL,
+	productId		VARCHAR(255) NOT NULL,
+	quantity		VARCHAR(255) NOT NULL
+);
+
+# --- !Downs
+
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS payment_method;
+DROP TABLE IF EXISTS paypal;
+DROP TABLE IF EXISTS credit_card;
+DROP TABLE IF EXISTS address;
+DROP TABLE IF EXISTS order;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS review;
+DROP TABLE IF EXISTS order_products;
