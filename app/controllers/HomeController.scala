4fcfffd4aeec6eff716ea1b20a2cee2a50a87b40
Braden Hutchinson <braden.hutchinson@hotmail.com>
2018-03-01 08:12:02 -0700

    Adding first controller and views

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,12 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+
+object HomeController extends Controller {
+
+  def index = Action {
+    Ok(views.html.index("Database Project"))
+  }
+
+}
