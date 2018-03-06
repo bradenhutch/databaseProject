96293182c50ac9f0842906ef2ead0b8560d19799
Braden Hutchinson <braden.hutchinson@hotmail.com>
2018-03-05 18:37:15 -0700

    some tweaks to adding a new product, added name field for products

--- app/controllers/ProductController.scala
+++ app/controllers/ProductController.scala
@@ -11,6 +11,11 @@ import models._
 class ProductController @Inject()(db: Database, cc: ControllerComponents) extends AbstractController(cc) {
 
   def create = Action(parse.urlFormEncoded) { implicit request =>
+
+  	//Validate all the data from the form
+
+  	//Extract all of the values from the form
+  	val productName = request.body("productName")(0)
   	val productPrice = request.body("productPrice")(0).toDouble
   	val productImageLocation = request.body("productImageLocation")(0)
   	val productColor = request.body("productColor")(0)
@@ -20,13 +25,14 @@ class ProductController @Inject()(db: Database, cc: ControllerComponents) extend
   	val productStock = request.body("productStock")(0).toInt
   	val productDescription = request.body("productDescription")(0)
 
+  	//Connect to the database and run the create query
   	implicit val conn = db.getConnection()
-  	val user = Product(None, productPrice, productImageLocation, productColor, 
+  	val user = Product(None, productName, productPrice, productImageLocation, productColor, 
   		productMaterial, productDimensions, productWeight, productStock, 
   		productDescription).create
   	println("*********************************************************")
   	conn.close()
-  	Ok
+  	Ok(views.html.admin())
   }
 
 }
--- app/models/product.scala
+++ app/models/product.scala
@@ -14,6 +14,7 @@ import javax.inject._
 
 /**
  * @param Id
+ * @param name
  * @param price
  * @param imageLocation
  * @param color
@@ -24,12 +25,21 @@ import javax.inject._
  * @param description
  */ 
 
-case class Product(Id: Option[Long], price: Double, imageLocation: String, 
+case class Product(Id: Option[Long], name: String, price: Double, imageLocation: String, 
 	color: String, material: String, dimensions: String, weight: Double, currentStock: Int, 
 	description: String) {
 
 	def create(implicit db:java.sql.Connection) = {
 		println("######################################")
-		SQL("INSERT INTO product (price, imageLocation, color, material, dimensions, weight, currentStock, description) VALUES ({price}, {imageLocation}, {color}, {material}, {dimensions}, {weight}, {currentStock}, {description});").on('price -> price, 'imageLocation -> imageLocation, 'color -> color, 'material -> material, 'dimensions -> dimensions, 'weight -> weight, 'currentStock -> currentStock, 'description -> description).executeInsert()
+		SQL("""
+			INSERT INTO product (name, price, imageLocation, color, material, 
+			dimensions, weight, currentStock, description) VALUES ({name}, {price}, 
+			{imageLocation}, {color}, {material}, {dimensions}, {weight}, 
+			{currentStock}, {description});
+			""").on(
+			'name -> name, 'price -> price, 'imageLocation -> imageLocation, 
+			'color -> color, 'material -> material, 'dimensions -> dimensions, 
+			'weight -> weight, 'currentStock -> currentStock, 
+			'description -> description).executeInsert()
 	}
 }
