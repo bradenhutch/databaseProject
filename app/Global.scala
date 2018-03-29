e77ee78bb305a3865305a6e6656fee26fcd72bb0
Braden Hutchinson <braden.hutchinson@hotmail.com>
2018-03-29 09:59:47 -0600

    re arranged some stuff and added error handling

--- app/Global.scala
+++ app/Global.scala
@@ -1,11 +1,22 @@
-/*import play.api.mvc._
+import play.api.http.HttpErrorHandler
+import play.api.mvc._
+import play.api.mvc.Results._
+import scala.concurrent._
+import javax.inject.Singleton
 
 
-object Global extends GlobalSettings {
+@Singleton
+class ErrorHandler extends HttpErrorHandler {
 
-	override def onStart(app: play.api.Application) {
-		// Add an initial test blog to the db
-		
+  def onClientError(request: RequestHeader, statusCode: Int, message: String) = {
+    Future.successful(
+      Status(statusCode)("A client error occurred: " + message)
+    )
+  }
 
-	}
-}*/
+  def onServerError(request: RequestHeader, exception: Throwable) = {
+    Future.successful(
+      InternalServerError("A server error occurred: " + exception.getMessage)
+    )
+  }
+}
