09743f6e742156aa9055656ebc0e7008266c8115
Braden Hutchinson <braden.hutchinson@hotmail.com>
2018-03-29 20:48:20 -0600

    Added view

--- triggerAndView.sql
+++ triggerAndView.sql
@@ -1,7 +1,11 @@
-#Trigger
+#Trigger and view
 
 DELIMITER //
 CREATE TRIGGER priceCheck
 BEFORE INSERT ON product
 FOR EACH ROW IF NEW.price < 0 THEN SET NEW.price = 0;
 END IF;//
+
+CREATE VIEW totalProducts AS
+	SELECT COUNT(*) FROM product;
+        
